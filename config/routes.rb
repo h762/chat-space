e520bf7897f58aef9741d5de0b40fe03a211f95c
h762 <11020i11020@gmail.com>
2020-06-27 22:09:39 +0900

    under making chat function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
+  root 'chats#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :chats, only: [:index, :create]
+  end
 end
