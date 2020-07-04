fdf4785415b6965affe926b4f2d630250562d003
h762 <11020i11020@gmail.com>
2020-07-04 13:43:31 +0900

    made an AutoReloadSistem

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(chat){
     if ( chat.image ){
       let html =
-        `<div class="Base__chat__display__info">
+        `<div class="Base__chat__display__info" data-chat-id=&{chat.id}>
           <div class="Base__chat__display__info__name">
             ${chat.user_name}
           </div>
@@ -19,7 +19,7 @@ $(function(){
       return html;
     }else {
       let html = 
-        `<div class="Base__chat__display__info">
+        `<div class="Base__chat__display__info" data-chat-id=&{chat.id}>
         <div class="Base__chat__display__info__name">
           ${chat.user_name}
         </div>
@@ -36,30 +36,6 @@ $(function(){
     };
   }
 
-  $("[class^='Form']").on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      let html = buildHTML(data);
-      $('.Base__chat__display').append(html);
-      $('.Base__chat__display').animate({ scrollTop: $('.Base__chat__display')[0].scrollHeight});
-      $('Form')[0].reset();
-      $('.Base__chat__form__area__sendBtn').prop('disabled', false);
-    })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    });
-  });
-
   let reloadChats = function() {
     let last_chat_id = $('.Base__chat__display__info:last').data("chat-id");
     $.ajax({
@@ -69,10 +45,18 @@ $(function(){
       data: {id: last_chat_id}
     })
     .done(function(chats) {
-      console.log('success');
+      if (chats.length !== 0) {
+        let insertHTML = '';
+        $.each(chats, function(i, chat) {
+          insertHTML += buildHTML(chat)
+        });
+        $('.Base__chat__display').append(insertHTML);
+        $('.Base__chat__display').animate({ scrollTop: $('.Base__chat__display')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
     });
   };
+  setInterval(reloadChats, 7000);
 });
